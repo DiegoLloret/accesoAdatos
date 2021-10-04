7325ba978849b47c6f5755a0655f24f93a0ddf75
Diego <Diego@DIEGO-PC>
2021-10-04 12:07:53 +0200

    tarea 4 "hecha"

--- AccesoADatos/src/tema1/Tarea4.java
+++ AccesoADatos/src/tema1/Tarea4.java
@@ -2,29 +2,46 @@ package tema1;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 
 public class Tarea4 {
 	public static void main(String[] args) throws InterruptedException {
+		File carpeta = null;
+		FileInputStream t = null;
 		try {
-			if(args.length==0) {
+			if (args.length == 0) {
 				System.out.println("no has pasado parametros");
 				return;
-			}else {
-				System.out.println(" has pasado como parametros: "+args[0]);
+			} else {
+				System.out.println(" has pasado como parametros: " + args[0]);
 			}
-			File carpeta = new File(args[0]);
-			int ch;
-			Reader reader = new StringReader(args[0]);
-			FileInputStream t= new FileInputStream(carpeta);
-			byte[] buffer= new byte[(int)carpeta.length()];
+			carpeta = new File(args[0]);
+			t = new FileInputStream(carpeta);
+			byte[] buffer = new byte[(int) carpeta.length()];
+			// try {
 			t.read(buffer);
+			// } catch (IOException e1) {
+			// System.out.println("Error al leer el fichero "+args[0]+"\n"+e1);
+			// }
 			t.close();
-			
-			   System.out.print(new String (buffer));
-		} catch (Exception e) {
-			System.out.println(e);
+
+			System.out.print(new String(buffer, StandardCharsets.UTF_8));
+		} catch (FileNotFoundException e) {
+			System.out.println("Fichero no encontrado " + args[0] + "\n");
+
+		} catch (IOException e) {
+			System.out.println("Error al leer el fichero " + args[0] + "\n" + e);
+			try {
+				t.close();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("Error al cerrar el fichero " + args[0] + "\n" + e1);
+			}
 		}
+
 	}
 }
